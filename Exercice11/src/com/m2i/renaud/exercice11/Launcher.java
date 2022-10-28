80dd77dd782903d9daafaf84c3191d61b586091d
sebas <sebas@DESKTOP-TB5HCU8.lan>
2022-10-28 10:37:44 +0200

    On essaie de modif

--- Exercice11/src/com/m2i/renaud/exercice11/Launcher.java
+++ Exercice11/src/com/m2i/renaud/exercice11/Launcher.java
@@ -5,22 +5,24 @@ import java.util.Scanner;
 public class Launcher {
 
 	public static void main(String[] args) {
-		// Demandez à l'utilisateur trois textes et présentez lui ensuite : 
-		// il s'agit d'une liste de choses à ne pas oublier pour la journée.
+		// Demandez ï¿½ l'utilisateur trois textes et prï¿½sentez lui ensuite : 
+		// il s'agit d'une liste de choses ï¿½ ne pas oublier pour la journï¿½e.
 		Scanner scanner = new Scanner(System.in);
 		String[] taches = new String[5];
 		
 		// Partie "Remplissage du tableau"
-		System.out.println("Donnez les " + taches.length + " tâches à ne pas oublier aujourd'hui :");
+		System.out.println("Donnez les " + taches.length + " tï¿½ches ï¿½ ne pas oublier aujourd'hui :");
 		for(int i = 0; i < taches.length; i++) {
 			taches[i] = scanner.nextLine();
 		}
 		
 		// Partie "Affichage du tableau"
-		System.out.println("Voici les tâches à ne pas oublier :");
+		System.out.println("Voici les tï¿½ches ï¿½ ne pas oublier :");
 		for(int i = 0; i < taches.length; i++) {
 			System.out.println(" - " + taches[i]);
 		}
+		
+		// On test le push
 		
 		scanner.close();
 
